77101d2be193d78a812e55f536496ebcb3b6f936
1533511302 <34854372+1533511302@users.noreply.github.com>
2018-11-20 23:37:40 +0800

    加注释

--- src/top/maniy/proxy/Client.java
+++ src/top/maniy/proxy/Client.java
@@ -8,12 +8,13 @@ package top.maniy.proxy;
  */
 public class Client {
     public static void main(String[] args) {
+        //普通的方式
         Car car =new Car();
         car.move();
-
+        //继承的方式
         MoveAble car2 =new Car2();
         car.move();
-
+        //聚合的方式
         MoveAble car3 =new Car3(car);
         car3.move();
     }
